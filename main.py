e852830b14782111eccb7985ecc06b15f1ecbb4f
perez1187 <o.perez1187@gmail.com>
2022-05-15 11:48:08 +0200

    pro_function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv(r"data\active_03_05_22.csv")
-df2 = pd.read_csv(r"data\oferta_20220503.csv")
-df_main = pd.concat([df,df2])
-df_main['is_ok']=df_main.duplicated(subset=['match','bet', 'outcome'])
-#df_main.to_csv(r'data/my_data3.csv')
-print (df_main)
+def find_duplicates():
+    df = pd.read_csv(r"data\active15.csv")
+    df2 = pd.read_csv(r"data\candidates15.csv")
+    df_main = pd.concat([df,df2])
+    df_main['is_ok']=df_main.duplicated(subset=['match','bet', 'outcome'])
+    df_main.to_csv(r'data/my_data150522.csv')
+    print (df_main)
 
-# todo chuj chuj
+def __main__():
+    find_duplicates()
+
+if __name__ == "__main__":
+    __main__()
+
